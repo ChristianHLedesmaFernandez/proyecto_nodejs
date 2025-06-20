812b6b6534ec6bc40ba91174b7d5c9878d1fab08
Christian H. Ledesma <christianhledesmafernandez@gmail.com>
2025-06-20 16:05:54 -0300

    Agregando estructura de carpeta

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 import express from "express";
 const app = express();
 
+// Lista de Prueba
+
+const productos = [
+                {id: 1,   nombre: "Cafe con leche",           precio: 12}, 
+                {id: 2,   nombre: "Capuchino",                precio: 8}, 
+                {id: 3,   nombre: "Media Luna",               precio: 19890}, 
+                {id: 4,   nombre: "Tostado de JyQ",           precio: 100}, 
+                {id: 5,   nombre: "Te",                       precio: 8},
+                {id: 6,   nombre: "Licuado de Frutos Rojos",  precio: 182}, 
+                {id: 7,   nombre: "Jugo de Limon",            precio: 48},
+                {id: 8,   nombre: "Mil Hojas",                precio: 98},
+                {id: 9,   nombre: "Panini",                   precio: 190}, 
+                {id: 10,  nombre: "Torta de Ricota",          precio: 100}, 
+                {id: 11,  nombre: "Te Negro",                 precio: 8},
+                {id: 12,  nombre: "Selva Negra",              precio: 56},
+                {id: 13,  nombre: "Te con leche",             precio: 15}, 
+                {id: 14,  nombre: "Cortado",                  precio: 8}, 
+                {id: 15,  nombre: "Lemon Pie",                precio: 19890}, 
+                {id: 16,  nombre: "Media Luna de JyQ",        precio: 100}, 
+                {id: 17,  nombre: "Moca",                     precio: 12}, 
+                {id: 18,  nombre: "Submarino",                precio: 8},
+                {id: 19,  nombre: "Red Velvet",               precio: 890}, 
+                {id: 20,  nombre: "Pebete de JyQ",            precio: 100}, 
+                {id: 21,  nombre: "Licuado",                  precio: 8},
+                {id: 22,  nombre: "Torta de Ricota",          precio: 28}             
+                ]
+
+//
+
 // Middleware de aplicación
 app.use((req, res, next) => {
     console.log(`Datos recibido: ${req.method} ${req.url}`);
@@ -14,15 +43,21 @@ app.get('/', (req, res) => {
 });
 
 
-
 /*
-app.get('/productos', (req, res) => { 
-res.send('Bienvenid@ a la página de productos'); 
-}); 
-app.get('/productos/14', (req, res) => { 
-res.send('Estás viendo el producto N° 14.'); 
-}); 
 */
+
+
+app.get('/productos', (req, res) => { 
+    res.json(productos); 
+});
+
+
+app.get('/productos/:id', (req, res) => { 
+    const producto = productos.find((item) => item.id == req.params.id);
+    console.log(req.params.id)
+    res.json(producto); 
+});
+
 
 const PORT = 3000;
 
